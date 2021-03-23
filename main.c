2a15bbf35d5deed45b57ffdb77bd57a976f375f0
audreytfr <audrey.tuffier@groupe-esigelec.org>
2021-03-23 10:50:22 +0100

    Update comments

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ volatile unsigned char RXDta;
  */
 void main( void )
 {
-    // Stop watchdog timer to prevent time out reset
+    /* Stop watchdog timer to prevent time out reset */
     WDTCTL = WDTPW | WDTHOLD;
 
     if(CALBC1_1MHZ==0xFF || CALDCO_1MHZ==0xFF)
@@ -37,70 +37,70 @@ void main( void )
     }
     else
     {
-        // Factory Set.
+        /* Factory Set. */
         DCOCTL = 0;
         BCSCTL1 = CALBC1_1MHZ;
         DCOCTL = (0 | CALDCO_1MHZ);
     }
 
-    //--------------- Secure mode
-    P1SEL = 0x00;        // GPIO
-    P1DIR = 0x00;         // IN
+    /*--------------- Secure mode */
+    P1SEL = 0x00;   /* GPIO */
+    P1DIR = 0x00;   /* IN */
 
-    // led
+    /* led */
     P1DIR |=  BIT0;
     P1OUT &= ~BIT0;
 
-    // USI Config. for SPI 3 wires Slave Op.
-    // P1SEL Ref. p41,42 SLAS694J used by USIPEx
+    /* USI Config. for SPI 3 wires Slave Op. */
+    /* P1SEL Ref. p41,42 SLAS694J used by USIPEx */
     USICTL0 |= USISWRST;
     USICTL1 = 0;
 
-    // 3 wire, mode Clk&Ph / 14.2.3 p400
-    // SDI-SDO-SCLK - LSB First - Output Enable - Transp. Latch
+    /* 3 wire, mode Clk&Ph / 14.2.3 p400 */
+    /* SDI-SDO-SCLK - LSB First - Output Enable - Transp. Latch */
     USICTL0 |= (USIPE7 | USIPE6 | USIPE5 | USILSB | USIOE | USIGE );
-    // Slave Mode SLAU144J 14.2.3.2 p400
+    /* Slave Mode SLAU144J 14.2.3.2 p400 */
     USICTL0 &= ~(USIMST);
     USICTL1 |= USIIE;
     USICTL1 &= ~(USICKPH | USII2C);
 
-    USICKCTL = 0;           // No Clk Src in slave mode
-    USICKCTL &= ~(USICKPL | USISWCLK);  // Polarity - Input ClkLow
+    USICKCTL = 0;   /* No Clk Src in slave mode */
+    USICKCTL &= ~(USICKPL | USISWCLK);  /* Polarity - Input ClkLow */
 
     USICNT = 0;
-    USICNT &= ~(USI16B | USIIFGCC ); // Only lower 8 bits used 14.2.3.3 p 401 slau144j
-    USISRL = 0x23;  // hash, just mean ready; USISRL Vs USIR by ~USI16B set to 0
+    USICNT &= ~(USI16B | USIIFGCC );    /* Only lower 8 bits used 14.2.3.3 p 401 slau144j */
+    USISRL = 0x23;                      /* hash, just mean ready; USISRL Vs USIR by ~USI16B set to 0 */
     USICNT = 0x08;
 
-    // Wait for the SPI clock to be idle (low).
+    /* Wait for the SPI clock to be idle (low). */
     while ((P1IN & BIT5)) ;
 
     USICTL0 &= ~USISWRST;
 
-    __bis_SR_register(LPM4_bits | GIE); // general interrupts enable & Low Power Mode
+    __bis_SR_register(LPM4_bits | GIE); /* general interrupts enable & Low Power Mode */
 }
 
-// --------------------------- R O U T I N E S   D ' I N T E R R U P T I O N S
+/* --------------------------- R O U T I N E S   D ' I N T E R R U P T I O N S */
 
-/* ************************************************************************* */
-/* VECTEUR INTERRUPTION USI                                                  */
-/* ************************************************************************* */
+/* *************************************************************************** */
+/* VECTEUR INTERRUPTION USI                                                    */
+/* *************************************************************************** */
 #pragma vector=USI_VECTOR
 __interrupt void universal_serial_interface(void)
 {
-    while( !(USICTL1 & USIIFG) );   // waiting char by USI counter flag
+    while( !(USICTL1 & USIIFG) );       /* waiting char by USI counter flag */
     RXDta = USISRL;
 
-    if (RXDta == 0x31) //if the input buffer is 0x31 (mainly to read the buffer)
+    if (RXDta == 0x31)                  /* if the input buffer is 0x31 (mainly to read the buffer) */
     {
-        P1OUT |= BIT0; //turn on LED
+        P1OUT |= BIT0;                  /* turn on LED */
     }
     else if (RXDta == 0x30)
     {
-        P1OUT &= ~BIT0; //turn off LED
+        P1OUT &= ~BIT0;                 /* turn off LED */
     }
     USISRL = RXDta;
-    USICNT &= ~USI16B;  // re-load counter & ignore USISRH
-    USICNT = 0x08;      // 8 bits count, that re-enable USI for next transfert
+    USICNT &= ~USI16B;                  /* re-load counter & ignore USISRH */
+    USICNT = 0x08;                      /* 8 bits count, that re-enable USI for next transfert */
 }
-//------------------------------------------------------------------ End ISR
+/* ------------------------------------------------------------------- End ISR */
